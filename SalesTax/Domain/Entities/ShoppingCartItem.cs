abefac0a58b6a093506f51f71477a530c1e1f1c1
Godfrey Marosha <godfreymarosha26@gmail.com>
2021-06-12 01:44:42 +0200

    Shopping Cart Item

---
+++ SalesTax/Domain/Entities/ShoppingCartItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class ShoppingCartItem
+    {
+        private readonly SalesTaxCalculator _salesTaxCalculator;
+
+        public ShoppingCartItem(SalesTaxCalculator salesTaxCalculator)
+        {
+            if (salesTaxCalculator == null)
+            {
+                throw new ArgumentException("Shopping Cart Item is invalid");
+            }
+
+            _salesTaxCalculator = salesTaxCalculator;
+        }
+
+        public decimal GetItemPrice()
+        {
+            return _salesTaxCalculator.GetPriceWithTaxIncluded(this);
+        }
+    }
+}
