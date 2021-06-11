f2b99db46f52f43eba96155cceeed722302126bf
Godfrey Marosha <godfreymarosha26@gmail.com>
2021-06-12 01:48:35 +0200

    Sales Tax Calculator
    The rounding rules for tax are:
    For a tax rate of n%, a shelf price of p contains (np / 100 rounded up to the nearest 0.05) amount of
    tax.

---
+++ SalesTax/Domain/SalesTaxCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Domain.Entities;
+
+namespace Domain
+{
+    abstract public class SalesTaxCalculator
+    {
+        public abstract decimal GetTaxAmount(ShoppingCartItem product);
+
+        public decimal GetPriceWithTaxIncluded(ShoppingCartItem product)
+        {
+            return product;
+        }
+
+        private decimal RoundUp(decimal amount)
+        {
+            //0.05 is 1/20
+            return Math.Ceiling(amount * 20)/20;
+        }
+    }
+}
