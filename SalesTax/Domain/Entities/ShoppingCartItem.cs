a646d04b97d887ca46a4c5632cda0ce6e38b300f
Godfrey Marosha <godfreymarosha26@gmail.com>
2021-06-12 02:03:44 +0200

    Shopping Cart Item
    Updated.

--- SalesTax/Domain/Entities/ShoppingCartItem.cs
+++ SalesTax/Domain/Entities/ShoppingCartItem.cs
@@ -5,15 +5,21 @@ namespace Domain.Entities
     public class ShoppingCartItem
     {
         private readonly SalesTaxCalculator _salesTaxCalculator;
+        public decimal BasePrice { get; private set; }
+        public Product Product { get; private set; }
+        public bool IsImported { get; private set; }
 
-        public ShoppingCartItem(SalesTaxCalculator salesTaxCalculator)
+        public ShoppingCartItem(SalesTaxCalculator salesTaxCalculator, Product product, bool isImported, decimal basePrice)
         {
-            if (salesTaxCalculator == null)
+            if (salesTaxCalculator == null || product == null || basePrice < 0)
             {
                 throw new ArgumentException("Shopping Cart Item is invalid");
             }
 
             _salesTaxCalculator = salesTaxCalculator;
+            Product = product;
+            BasePrice = basePrice;
+            IsImported = isImported;
         }
 
         public decimal GetItemPrice()
